96079464ddc24379a2410926008b3a89cd95ce90
Shaunak Mishra <shaunakmishra374@gmail.com>
2025-12-03 11:34:29 +0530

    Two Pointer Questions

---
+++ Array/RemoveElement.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n =  nums.length, j = 0;
+        for(int i = 0; i<n; i++) if(nums[i]!=val) nums[j++] = nums[i];
+        return j;
+    }
+}
