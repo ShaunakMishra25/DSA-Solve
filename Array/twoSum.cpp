a4084ccd1b3ae513c89f5aff6c7da84d9e9855e6
Shaunak Mishra <shaunakmishra374@gmail.com>
2025-12-04 22:41:48 +0530

    Two Sum

--- Array/twoSum.cpp
+++ Array/twoSum.cpp
@@ -6,6 +6,7 @@ public:
         for(int i = 0; i < nums.size(); ++i) {
             int diff = target - nums[i];
             if(mp.find(diff) != mp.end()) {
+                
                 return {mp[diff], i};
             }
             mp[nums[i]] = i;
