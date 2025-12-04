a73fcdbe009bada97acf3ad6fde813a28d3da643
Shaunak Mishra <shaunakmishra374@gmail.com>
2025-12-04 17:28:46 +0530

    Questions on Two pointer

---
+++ Array/PlusOne.java
@@ -0,0 +1,30 @@
+public class PlusOne {
+    public static void main(String[] args) {
+        Solution s1 = new Solution();
+        int[] digits = {9,9,9};
+        int[] result = s1.plusOne(digits);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        int i =n-1;
+        while(i>=0){
+            if(digits[i]<9){
+                digits[i]+=1;
+                return digits;
+            }
+            else {
+                digits[i] = 0;
+                i--;
+            }
+
+        }
+            int res[] = new int[n+1];
+            res[0] =1;
+            return res;   
+    }
+}
