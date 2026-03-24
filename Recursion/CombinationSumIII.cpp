39e7f54d947772f12fc6e3d40526a2c549ea8a85
ShaunakMishra25 <shaunakmishra374@gmail.com>
2026-03-25 00:03:38 +0530

    Backtracking

---
+++ Recursion/CombinationSumIII.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> nums = {1,2,3,4,5,6,7,8,9};
+        vector<vector<int>> res;
+        vector<int> curr;
+
+        solve(nums, k, n, 0, 0, 0, curr, res);
+        return res;
+    }
+    void solve(vector<int>& nums, int k, int n , int index, int count, int sum, vector<int> curr, vector<vector<int>> res){
+
+        if(sum == n && k == count){
+            res.push_back(curr);
+        }
+        if(sum > n || count > k || index >= nums.size()){
+            return;
+        }
+
+        curr.push_back(nums[index]);
+
+        solve(nums, k, n, index + 1, count + 1, sum + nums[index], curr, res);
+
+        solve(nums, k, n, index + 1, count, sum, curr, res);
+        curr.pop_back();
+
+        return;
+    }
+};
